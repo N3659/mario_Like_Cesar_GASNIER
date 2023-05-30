d3d9f5a6af5f2ae4a901562881b5d68e4cbf2c4d
N3659 <76428379+N3659@users.noreply.github.com>
2023-05-30 12:32:06 +0100

    Add files via upload

--- Menu.js
+++ Menu.js
@@ -8,14 +8,21 @@ export class Menu extends Phaser.Scene{
 
         this.load.image("Ecran_Menu", 'assets/menu.png')
         this.load.image("Bouton_Start", 'assets/commencer.png')
+
+        this.load.audio("Musique_Menu", 'assets/Music/Musique_Menu.mp3')
     }
 
     create () {
 
+        this.musiqueDeFond = this.sound.add("Musique_Menu");
+        this.musiqueDeFond.play();
     
         this.add.image(640,355, "Ecran_Menu").setScale(0.659);
         this.add.image(600, 600, "Bouton_Start").setScale(0.4).setInteractive();
         this.input.on('pointerdown', () => {this.click = true});
+
+        
+
     }
 
     update(){
@@ -23,10 +30,14 @@ export class Menu extends Phaser.Scene{
             this.LancementJeu();
             this.click = false;
         }
+
     }
+    
     LancementJeu(){
 
-        this.scene.start('Niveau_Deni')
+        this.musiqueDeFond.stop();
+
+        this.scene.start('Niveau_Depression')
 
     }
 
